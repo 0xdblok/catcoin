da5f7eb9644640211fe653169666a780faa4a269
dblok0x <dblok0x@gmail.com>
2024-11-16 15:33:19 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -67,8 +67,8 @@ export default function Home() {
         </p>
       </div>
       
-    </div><h2 class="mt-16 text-5xl  tracking-tighter leading-none text-center text-white max-md:mt-10">
-  CA :<p className="pt-6 font-light text-zinc-200 text-base">TBA</p> 
+    </div><h2 class="mt-16 text-4xl  tracking-tighter leading-none text-center text-white max-md:mt-10">
+  CA :<p className="pt-6 font-light text-zinc-200 text-xl">0x4c20dFc91baDAdd93A2F392138f5d3b578338a9A</p> 
     </h2>
   
     <footer class="self-end mt-10 mr-11 text-base font-light text-zinc-400 max-md:mr-2.5">
